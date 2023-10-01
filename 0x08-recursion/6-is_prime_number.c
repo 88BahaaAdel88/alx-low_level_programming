5783c682daed31495cfae252aee520bb5aaecb1a
Bahaa <bahaaadel47880@gmail.com>
2023-10-01 10:54:26 +0300

    alias

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-int is_prime(int n , int c)
+/**
+ * is_prime - checks if n is a prime number
+ * @n: the tested number
+ * @c: number given to test n
+ * Return: recursion of is_prime till its over
+ */
+
+int is_prime(int n, int c)
 {
 	if (n % c == 0)
 	{
@@ -11,6 +18,12 @@ int is_prime(int n , int c)
 	}
 	return (is_prime(n, c + 1));
 }
+
+/**
+ * is_prime_number - checks if n is a valid number
+ * @n: number passed to the other function
+ * Return: the prime number
+ */
 
 int is_prime_number(int n)
 {
