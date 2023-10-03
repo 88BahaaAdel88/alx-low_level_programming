bc35dc39006fcfc6f0bd403794df883b6a38181d
Bahaa <bahaaadel47880@gmail.com>
2023-10-03 11:20:38 +0300

    alias

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,15 @@
 
 int main(int argc, char **argv)
 {
+	int num1, num2, i;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int i = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	i = num1 * num2;
 
 	printf("%d\n", i);
 
