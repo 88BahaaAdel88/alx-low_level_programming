41be865e9f76822d9614c540a9837332c2f22385
Bahaa <bahaaadel47880@gmail.com>
2023-10-10 00:28:20 +0300

    alias

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,15 @@
 #include "main.h"
+/**
+ * *malloc_checked - returns a ptr to the allocated memory
+ * @b: usr enter
+ * Return: the size assigned by malloc
+ */
 
 void *malloc_checked(unsigned int b)
 {
+	int *m = malloc(b);
 
+	if (m == 0)
+		exit(98);
+	return (m);
+}
