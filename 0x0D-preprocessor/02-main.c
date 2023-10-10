ef0331bc32db891bc41d3eb38abe283bda53d70c
Bahaa <bahaaadel47880@gmail.com>
2023-10-10 16:09:47 +0300

    alias

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints out the base file name
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
