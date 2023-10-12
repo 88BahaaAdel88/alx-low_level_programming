71b2a3b0e44411a7300e1da7b260acdbe649b750
Bahaa <bahaaadel47880@gmail.com>
2023-10-12 03:17:33 +0300

    alias

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include "main.h"
+/**
+ * struct dog - dog stuff
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dogs owner
+ *
+ * Description: dogs long description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
