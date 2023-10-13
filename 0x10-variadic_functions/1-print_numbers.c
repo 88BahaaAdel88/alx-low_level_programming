84281140522d758239e405e9338dcaf342cce40a
Bahaa <bahaaadel47880@gmail.com>
2023-10-14 02:44:32 +0300

    alias

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints nums with a separator
+ * @separator: sperator between strings
+ * @n: nums of arg
+ * @...: ints to print
+ *
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%d%s", va_arg(apm int),
+				i ? (separator ? separator : "") : "\n");
+va_end(ap);
+}
