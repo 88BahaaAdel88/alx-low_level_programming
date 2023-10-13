35c7f157cc5c309dce7be25590db6826a73ce0d6
Bahaa <bahaaadel47880@gmail.com>
2023-10-13 14:02:46 +0300

    alias

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two ints
+ * @a: the 1st int
+ * @b: the 2nd int
+ *
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two ints
+ * @a: the 1st int
+ * @b: the 2nd int
+ *
+ * Return: the diff
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - muliply 2 ints
+ * @a: the 1st int
+ * @b: the 2nd int
+ *
+ * Return: the product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides 2 ints
+ * @a: the 1st int
+ * @b: the 2nd int
+ *
+ * Return: idk tbh
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modul two ints
+ * @a: the 1st int
+ * @b: the 2nd int
+ *
+ * Return: the remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
