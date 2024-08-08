3914a33bb6b77cdee33bce8f9ef865fd7a569980
Bahaa <bahaaadel47880@gmail.com>
2024-08-08 15:55:01 +0300

    alias

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i = 0;
+	char *str, *separator = "";
+
+
+	va_start(ap, format);
+
+	while(format && format[i])
+	{
+		switch(format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+	}
+
+	va_end(ap);
+	printf("\n");
+}
