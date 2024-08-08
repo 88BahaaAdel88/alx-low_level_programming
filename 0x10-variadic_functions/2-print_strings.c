0febae967da833c604c8fa5d88a5e5a3953a19f9
Bahaa <bahaaadel47880@gmail.com>
2024-08-08 15:09:10 +0300

    alias

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,22 +9,23 @@
  * Return: Nothing
  */
 
-void print_strings(const char *separator, const unsigned int n, ...) {
-  unsigned int i = n;
-  va_list ap;
-    char* str;
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = n;
+	va_list ap;
+	char *str;
 
-    va_start(ap, n);
+	va_start(ap, n);
 
-    
-    for(i = 0; i < n; i++){
-        str = va_arg(ap, char*);
-        printf("%s", str ? str : "(nil)");
-        
-        if (separator && i < n - 1){
-            printf("%s", separator);
-        }
-    }
-    printf("\n");
-  va_end(ap);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		printf("%s", str ? str : "(nil)");
+		if (separator && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
 }
